d5506a1ce1eaa968e5cf0a42fdb182311b72fd79
SijiaMao <sijia501@gmail.com>
2021-04-03 00:49:55 -0400

    fixed img alt text :city_sunrise: :sunrise_over_mountains: :mount_fuji:

--- scripts.js
+++ scripts.js
@@ -46,7 +46,7 @@ $(document).ready(function() {
     
     // Character images
     const charaImg = {
-        img01: `<img src="./assets/char_01.png" alt="Picture of the character.Illustration of Exusiai looking confident.">`,
+        img01: `<img src="./assets/char_01.png" alt="Illustration of Exusiai looking confident.">`,
         img02: `<img src="./assets/char_02.png" alt="Illustration of Exusiai looking hushed.">`,
         img03: `<img src="./assets/char_03.png" alt="Illustration of Exusiai looking troubled.">`,
         img04: `<img src="./assets/char_04.png" alt="Illustration of Exusiai looking happy.">`
