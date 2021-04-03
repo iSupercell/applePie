f7e1420b9c01e1a04b2a9a6388b63d6f99a7d3b4
SijiaMao <sijia501@gmail.com>
2021-04-02 22:56:07 -0400

    finished responsive design and added more error handling :apple: :green_apple: :doughnut:

---
+++ README.md
@@ -0,0 +1,24 @@
+# Apple Pie!
+This is the very first project I made using jQuery! The main goal was to practice using jQuery to manipulate the HTML when users interacted with the app.
+
+I tried to create something that mimics the in-game dialogue mechanism. Unfortunately, story writing is not my thing so this ended up being a simple multiplication game.
+
+## How it works
+1. User can navigate the game by clicking on the < and > arrows.
+2. The text inside the dialogue box will change accordingly.
+3. The user will be given a choice between YES or NO (spoiler alert: One can only proceed upon choosing YES).
+4. The YES or NO option is disabled at other times to avoid confusion.
+5. The Character's expression will change accordingly.
+6. The numbers forming the math question in the dialogue text will be generated at random.
+7. The generated numbers will appear in the equation visual below the dialogue box.
+8. The user will be able to input and submit their answer.
+9. The submitted answer will be checked (right vs. wrong). 
+10. The character's expression and dialogue will be changed accordingly.
+11. A reroll button will appear once user has answered correctly.
+12. User can then choose to reroll for a new equation.
+NOTE: The input and submit button are disabled until the equation is generated to avoid errors.
+
+## Disclaimer
+I do not own the character illustrations used in this app. All credit goes to her amazing artist: 幻象黑兔 twitter @ https://twitter.com/coneyrivard
+Exusiai is also from a tower defense mobile game called Arknights. It's a great game and you should give it a shot. Yes, she does love apple pie. It's part of her lore.
+Find out more about Arknights: https://www.arknights.global/
--- scripts.js
+++ scripts.js
@@ -1,25 +1,11 @@
-// Overview of how it should work
-// ---------------------------- //
-// When user click on arrow left or right, it will change the text in the dialogue box
-// On the 3rd dialogue text, user will be asked to choose between YES or NO
-// The YES or NO option should be disabled at other times to avoid confusion
-// Character's expression should change depending on the dialogue text
-// The math question in the dialogue text will be generated randomly
-// The generated numbers will appear in the equation visual
-// Define and store user's input value to compare with the correct answer at each submit
-// Character's expression and dialogue will change depending on the user's answer (RIGHT vs WRONG)
-// Reroll button will reroll a new equation
-
-
-// ---------------------------- //
-// My codes starts below!!!
-
 $(document).ready(function() {
 
     // Some variable to use later
     let counter = 0;
     let answerApple;
     let rerollCorrect;
+    const screen = window.matchMedia(`(max-width: 768px)`);
+
 
     // RNG and Math functions to use later
     const generateApple = Math.floor(Math.random() * 10) + 1;
@@ -36,15 +22,28 @@ $(document).ready(function() {
 
     // Script for the dialogue box
     const charaConvo = {
-        default: `To get started, just click on the &#10095; arrow next to me!`,
+        default: `To get started, just click on the &#10095; arrow next to this text box!`,
         convo0: `Hey there! I need to buy enough apples to make some apple pie for tonight's party.`,
         convo1: `Can you help me find out how many apples I need to get?`,
         convo2: `Awww! Don't be like that. Help a friend out!`,
-        convo3: `Great! The receipe says that I need <span class="highlight">${generateApple} apple(s)</span> to make one apple pie. I have to make <span class="highlight">${generatePie} apple pie(s)</span> tonight so how many apples should I get?`,
+        convo3: generateConvo(generateApple, generatePie),
         convo4: `Hmmm... Are you sure about that?`,
         convo5: `I see! Thanks for the help. I'll make sure to save you a slice!`
     };
 
+    function generateConvo(getApple, getPie){
+        if (getApple === 1){
+            return `Great! The receipe says that I need <span class="highlight">${getApple} apple</span> to make one apple pie. I have to make <span class="highlight">${getPie} apple pies</span> tonight so how many apples should I get?`
+        } else if (getPie === 1){
+            return `Great! The receipe says that I need <span class="highlight">${getApple} apples</span> to make one apple pie. I have to make <span class="highlight">${getPie} apple pie</span> tonight so how many apples should I get?`
+        } else if (getApple && getPie === 1){
+            return `Great! The receipe says that I need <span class="highlight">${getApple} apple</span> to make one apple pie. I have to make <span class="highlight">${getPie} apple pie</span> tonight so how many apples should I get?`
+        } else {
+            return `Great! The receipe says that I need <span class="highlight">${getApple} apples</span> to make one apple pie. I have to make <span class="highlight">${getPie} apple pies</span> tonight so how many apples should I get?`
+        }
+    }
+
+    
     // Character images
     const charaImg = {
         img01: `<img src="./assets/char_01.png" alt="Picture of the character.Illustration of Exusiai looking confident.">`,
@@ -53,10 +52,18 @@ $(document).ready(function() {
         img04: `<img src="./assets/char_04.png" alt="Illustration of Exusiai looking happy.">`
     }
 
+    const charaImgMobile = {
+        img01: `<img src="./assets/char_01m.png" alt="Illustration of Exusiai looking confident.">`,
+        img02: `<img src="./assets/char_02m.png" alt="Illustration of Exusiai looking confident.">`,
+        img03: `<img src="./assets/char_03m.png" alt="Illustration of Exusiai looking confident.">`,
+        img04: `<img src="./assets/char_04m.png" alt="Illustration of Exusiai looking confident.">`
+    }
+
 
     // Default visual settings
     $('.ynbutton, .restart, .leftArrow, .applesNeeded, .pies').css('visibility', 'hidden');
     $("#answer:input").prop("disabled", true);
+    $('[type="submit"]').prop('disabled', true);
 
 
     // Code for the dialogue box toggle
@@ -78,30 +85,31 @@ $(document).ready(function() {
             $('.ynbutton, .leftArrow').css('visibility', 'hidden');
             $('.rightArrow').css('visibility', '');
             $('.dialogueText').html(charaConvo.default);
-            $('.character').html(charaImg.img01);
+            screen.matches ? $('.characterMobile').html(charaImgMobile.img01) : $('.character').html(charaImg.img01)
         } else if (counter == 1) {
             $('.ynbutton').css('visibility', 'hidden');
             $('.leftArrow, .rightArrow').css('visibility', '');
             $('.dialogueText').html(charaConvo.convo0);
-            $('.character').html(charaImg.img01);
+            screen.matches ? $('.characterMobile').html(charaImgMobile.img01) : $('.character').html(charaImg.img01)
         };
     });
 
 
     // Code for YES or NO button
     $('#y').on('click', function(){
-        $('.ynbutton, .leftArrow').css('visibility', 'hidden');
-        $('.restart, .applesNeeded, .pies').css('visibility', '');
-        $('.dialogueText').html(charaConvo.convo3);
+        $('.ynbutton, .leftArrow').addClass('hide');
+        $('.applesNeeded, .pies').css('visibility', '');
         $('.applesNeeded').html(generateApple);
         $('.pies').html(generatePie);
-        $('.character').html(charaImg.img01);
+        $('.dialogueText').html(charaConvo.convo3);
         $("#answer:input").prop("disabled", false);
+        $('[type="submit"]').prop('disabled', false);
+        screen.matches ? $('.characterMobile').html(charaImgMobile.img01) : $('.character').html(charaImg.img01)
     });
 
     $('#n').on('click', function(){
         $('.dialogueText').html(charaConvo.convo2);
-        $('.character').html(charaImg.img02);
+        screen.matches ? $('.characterMobile').html(charaImgMobile.img02) : $('.character').html(charaImg.img02)
     });
 
 
@@ -110,10 +118,11 @@ $(document).ready(function() {
         const rerollApple = newApple();
         const rerollPie = newPie();
         rerollCorrect = rerollApple * rerollPie;
-        $('.dialogueText').html(`Great! The receipe says that I need <span class="highlight">${rerollApple} apple(s)</span> to make one apple pie. I have to make <span class="highlight">${rerollPie} apple pie(s)</span> tonight so how many apples should I get?`);
+        $('.dialogueText').html(generateConvo(rerollApple, rerollPie));
         $('.applesNeeded').html(rerollApple);
         $('.pies').html(rerollPie);
-        $('.character').html(charaImg.img01);
+        $('.restart').css('visibility', 'hidden');
+        screen.matches ? $('.characterMobile').html(charaImgMobile.img01) : $('.character').html(charaImg.img01)
     });
 
 
@@ -130,13 +139,15 @@ $(document).ready(function() {
     function checkAnswer() {
         if (answerApple == correct) {
             $('.dialogueText').html(charaConvo.convo5);
-            $('.character').html(charaImg.img04);
+            $('.restart').css('visibility', '');
+            screen.matches ? $('.characterMobile').html(charaImgMobile.img04) : $('.character').html(charaImg.img04)
         } else if (answerApple == rerollCorrect) {
             $('.dialogueText').html(charaConvo.convo5);
-            $('.character').html(charaImg.img04);
+            $('.restart').css('visibility', '');
+            screen.matches ? $('.characterMobile').html(charaImgMobile.img02) : $('.character').html(charaImg.img02)
         } else {
             $('.dialogueText').html(charaConvo.convo4);
-            $('.character').html(charaImg.img03);
+            screen.matches ? $('.characterMobile').html(charaImgMobile.img03) : $('.character').html(charaImg.img03)
         }
     }
 
